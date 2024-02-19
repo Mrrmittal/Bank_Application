2f8b1045c010aa9642dbcb146eabec5c6a98d69c
Jatin Mittal <jatinmittal164@gmail.com>
2024-02-19 22:38:53 +0530

    Update

--- src/main/java/com/bankapplication/Bank_Application/Service/Operation.java
+++
@@ -1,6 +0,0 @@
-package com.bankapplication.Bank_Application.Service;
-
-public interface Operation {
-    public void bankInfo();
-    public void operation(int key);
-}
--- src/main/java/com/bankapplication/Bank_Application/Service/Process.java
+++
@@ -1,9 +0,0 @@
-package com.bankapplication.Bank_Application.Service;
-
-public interface Process {
-    public void openAccount();
-    public void demoAccount();
-    public void deposit();
-    public void withdraw();
-    public void checkBalance();
-}
